0bd85c6f056b01856fba9734f25d1b4a534ae78e
Tiago <tvhollowknight@gmail.com>
2024-11-21 16:38:22 -0300

    Made the program be able to run properly
    The game isn't fully working, but presents no run time errors

--- generate_code.rb
+++ generate_code.rb
@@ -1,17 +1,26 @@
 module CpuOponent
   @@generated_code = nil
-  @@possible_colors = %w[red blue brown yellow green pink purple white black]
+  @@plays = 0
+  @@possible_colors = %w[red blue brown yellow green pink purple white black cyan gray orange]
+  def self.plays
+    @@plays
+  end
+
+  def self.possible_colors
+    @@possible_colors
+  end
+
   def self.generate_code
     @@generated_code = @@possible_colors.sample(4)
   end
 
   def self.check_code(user_code)
     user_code_correctness = { user_code[0] => nil, user_code[1] => nil, user_code[2] => nil, user_code[3] => nil }
-    return "You've cracked the code!!!" if user_code == @@generated_code
+    return 'won' if user_code == @@generated_code
 
     user_code.each do |color|
       @@generated_code.each do |entry|
-        if color == entry && user_code.index(color) == @@generate_code.index(entry)
+        if color == entry && user_code.index(color) == @@generated_code.index(entry)
           user_code_correctness[color] = 'correct'
         end
         if color == entry && user_code.index(color) != @@generated_code.index(entry)
@@ -21,6 +30,7 @@ module CpuOponent
         end
       end
     end
+    @@plays += 1
     user_code_correctness
   end
 end
--- mastermind.rb
+++ mastermind.rb
@@ -1,11 +1,24 @@
 require_relative 'generate_code'
-valid_play = false
-until valid_play != false
-  valid_play = true
-  print 'Take a guess: '
-  user_code = gets.chomp.split
-  valid_play = false if user_code.length != 4
-  user_code.each do |color|
-    valid_play = false unless CpuOponent.possible_colors.include?(color.lowercase)
+game_over = false
+CpuOponent.generate_code
+until game_over == true || CpuOponent.plays >= 12
+  valid_play = false
+  until valid_play != false
+    valid_play = true
+    print 'Take a guess: '
+    user_code = gets.chomp.split
+    valid_play = false if user_code.length != 4
+    user_code.each do |color|
+      valid_play = false unless CpuOponent.possible_colors.include?(color.downcase)
+    end
+  end
+  current_game = CpuOponent.check_code(user_code)
+  if current_game == 'won'
+    game_over = true
+    break
+  else
+    correct_guesses = current_game.values.count('correct')
+    misplaced_guesses = current_game.values.count('misplaced')
+    puts "You've got #{correct_guesses} correct guesses and #{misplaced_guesses} misplaced guesses"
   end
 end
