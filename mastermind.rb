a37d59f631be5784ed955ac496b132dc1891d275
Tiago <tvhollowknight@gmail.com>
2024-11-21 23:09:12 -0300

    Fully implemented the program for playing against the cpu
    Users can now play a mastermind round against the cpu

--- mastermind.rb
+++ mastermind.rb
@@ -1,24 +1,31 @@
 require_relative 'generate_code'
-game_over = false
+require 'colorize'
+game_won = false
 CpuOponent.generate_code
-until game_over == true || CpuOponent.plays >= 12
+until game_won == true || CpuOponent.plays >= 12
   valid_play = false
   until valid_play != false
     valid_play = true
     print 'Take a guess: '
     user_code = gets.chomp.split
-    valid_play = false if user_code.length != 4
+    valid_play = false if user_code.uniq.length != 4
     user_code.each do |color|
       valid_play = false unless CpuOponent.possible_colors.include?(color.downcase)
     end
   end
   current_game = CpuOponent.check_code(user_code)
   if current_game == 'won'
-    game_over = true
+    game_won = true
     break
   else
     correct_guesses = current_game.values.count('correct')
     misplaced_guesses = current_game.values.count('misplaced')
+    user_code.each do |color|
+      print color.colorize(color.to_sym) << ' '
+    end
+    puts ''
     puts "You've got #{correct_guesses} correct guesses and #{misplaced_guesses} misplaced guesses"
   end
 end
+puts "You've cracked the code!!!" if game_won
+puts 'You have no more guesses, game lost.' unless game_won
